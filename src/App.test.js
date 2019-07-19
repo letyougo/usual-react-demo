98aecc2a1b4459c8eb0f3cc43033e9e040be7765
surui1 <surui1@xiaomi.com>
2019-07-19 17:47:41 +0800

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import HashHistory from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<HashHistory />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
